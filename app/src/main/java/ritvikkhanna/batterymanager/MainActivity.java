deeebea92653e85d0650ead168bb46d2603476a4
Ritvik Khanna <ritvikkhanna09@gmail.com>
2017-08-06 00:54:53 +0530

    created custom dialog

--- app/src/main/java/ritvikkhanna/batterymanager/MainActivity.java
+++ app/src/main/java/ritvikkhanna/batterymanager/MainActivity.java
@@ -6,11 +6,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputFilter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
@@ -50,38 +52,81 @@ public class MainActivity extends AppCompatActivity {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                changeBattery();
+                Object o = lv.getItemAtPosition(i);
+                String str=(String)o;
+                String[] parts = str.split("-------------->");
+                String name = parts[0];
+                String battery = parts[1];
+                //changeBattery(name,battery);
+                showChangeLangDialog(name,battery);
+                Toast.makeText(getApplicationContext(), name+","+battery , Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-
-    void changeBattery(){
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference();
-        final DatabaseReference childRef = myRef.child("members").push();
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this,R.style.MyDialogTheme);
-        alertDialog.setTitle("Add Member");
-        alertDialog.setMessage("Enter Name");
-        final EditText et_members = new EditText(MainActivity.this);
-        et_members.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
-        alertDialog.setView(et_members);
-        alertDialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                String new_member = et_members.getText().toString();
-                Member member= new Member(new_member, "10");
-                try {
-
-                    childRef.setValue(member);
-                }
-                catch(Exception e){
-                    Toast.makeText(getApplicationContext(), e.toString() , Toast.LENGTH_SHORT).show();
-                }
+    public void showChangeLangDialog(final String name, final String battery) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this,R.style.MyDialogTheme);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.custom_dialog, null);
+        dialogBuilder.setView(dialogView);
+        final TextView edt = (TextView) dialogView.findViewById(R.id.text1);
+        edt.setText(battery);
+        final FloatingActionButton addt=(FloatingActionButton)dialogView.findViewById(R.id.fab_add);
+        final FloatingActionButton subt=(FloatingActionButton)dialogView.findViewById(R.id.fab_sub);
+        dialogBuilder.setTitle("Change Battery");
+        dialogBuilder.setMessage(name);
+        addt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String a = edt.getText().toString();
+                int bat_temp=Integer.parseInt(a);
+                bat_temp++;
+                edt.setText(Integer.toString(bat_temp));
             }
         });
-        alertDialog.setNegativeButton("Cancel", null);
-        AlertDialog dialog = alertDialog.create();
-        dialog.show();
+        subt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String a = edt.getText().toString();
+                int bat_temp=Integer.parseInt(a);
+                bat_temp--;
+                edt.setText(Integer.toString(bat_temp));
+            }
+        });
+        dialogBuilder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //do something with edt.getText().toString();
+                String a = edt.getText().toString();
+                //updateBattery(name,a);
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //pass
+            }
+        });
+        AlertDialog b = dialogBuilder.create();
+        b.show();
+    }
+
+
+    //updating battery
+    void updateBattery(String name,String battery){
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference();
+        final DatabaseReference childRef = myRef.child("members");
+
+        childRef.orderByChild("name").equalTo(name).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     //floating action button onClick calls this function
@@ -148,8 +193,9 @@ public class MainActivity extends AppCompatActivity {
         //names.clear();
         for (DataSnapshot data : ds.getChildren()) {
             Member m = new Member(data.getValue(Member.class).name, data.getValue(Member.class).battery);
-            if(!names.contains(m.name)){
-            names.add(m.name);}
+
+            if(!names.contains(m.dis)){
+            names.add(m.dis);}
         }
         ArrayAdapter adapter=new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,names);
         lv.setAdapter(adapter);
@@ -160,15 +206,17 @@ public class MainActivity extends AppCompatActivity {
     public static class Member{
         String name;
         String battery;
+        String dis;
         public Member(){
 
         }
         public Member(String name, String battery){
             this.name=name;
             this.battery=battery;
+            this.dis=name+"-------------->"+battery;
         }
     }
-    //Extra 
+    //Extra
     private void displayData(){
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference myRef = database.getReference();
